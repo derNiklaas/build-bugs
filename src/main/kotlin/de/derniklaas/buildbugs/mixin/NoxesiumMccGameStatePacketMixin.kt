91611b5af1f2799b77a76f02834c506ef2df69ec
Niklas <derNiklaas@users.noreply.github.com>
2024-01-07 21:18:36 +0100

    formatting

--- src/main/kotlin/de/derniklaas/buildbugs/mixin/NoxesiumMccGameStatePacketMixin.kt
+++ src/main/kotlin/de/derniklaas/buildbugs/mixin/NoxesiumMccGameStatePacketMixin.kt
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 @Mixin(ClientboundMccGameStatePacket::class)
-class NoxesiumMccGameStatePacketMixin {
+abstract class NoxesiumMccGameStatePacketMixin {
 
     @Inject(at = [At("HEAD")], method = ["receive"])
     fun receive(player: ClientPlayerEntity, responseSender: PacketSender, info: CallbackInfo) {
--- src/main/kotlin/de/derniklaas/buildbugs/mixin/NoxesiumMccServerPacketMixin.kt
+++ src/main/kotlin/de/derniklaas/buildbugs/mixin/NoxesiumMccServerPacketMixin.kt
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
 
 @Mixin(ClientboundMccServerPacket::class)
-class NoxesiumMccServerPacketMixin {
+abstract class NoxesiumMccServerPacketMixin {
 
     @Inject(at = [At("HEAD")], method = ["receive"])
     fun receive(player: ClientPlayerEntity, responseSender: PacketSender, info: CallbackInfo) {
